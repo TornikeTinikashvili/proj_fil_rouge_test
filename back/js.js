04a041001222c7ee86e50296cd8b06c0e7f39735
TornikeTinikashvili <ttttornike@gmail.com>
2023-01-08 22:46:13 +0100

    asd

--- back/js.js
+++ back/js.js
@@ -1,7 +1,7 @@
 
 // MODAL
 let openModalButtons = document.querySelectorAll('[data-modal-target]')
-let closeModalButtons = document.querySelectorAll('[data-modal-close]')
+let closeModalButtons = document.querySelectorAll('[data-close-button]')
 let overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
@@ -13,7 +13,7 @@ openModalButtons.forEach(button => {
 
 closeModalButtons.forEach(button => {
     button.addEventListener('click',() => {
-        let modal = button.closest('.accModal')
+        let modal = button.closest('.modal')
         closeModal(modal)
     })
 })
@@ -29,11 +29,24 @@ function closeModal(modal){
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
-
-// navigation
+// navbar
 let toggleButton = document.getElementsByClassName('toggle-button')[0]
-let navbarLinks = document.getElementsByClassName('nav-section')[0]
+let navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+// hide navbar when scrolling 
+var lastScrollTop;
+navbar = document.querySelector('.navbar');
+window.addEventListener('scroll',function(){
+var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+if(scrollTop > lastScrollTop){
+navbar.style.top='-122px';
+}
+else{
+navbar.style.top='0';
+}
+lastScrollTop = scrollTop;
+});
